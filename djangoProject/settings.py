d6a5e9686432a723eebdfa8d5c4389d715491ecd
AbdusalomovIbroxim <ibrohimlololowka@gmail.com>
2025-07-19 21:16:11 +0500

    update: allow cors

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -186,11 +186,13 @@ SWAGGER_SETTINGS = {
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_CREDENTIALS = True
-
+# CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "https://polya-top-boy-frontend.onrender.com",
+]
 # CORS_ALLOW_ALL_ORIGINS = True
 
 # CORS_ALLOWED_ORIGINS = os.getenv('ALLOWED_ORIGINS', '').split(',')
-CORS_ALLOWED_ORIGINS = "https://polya-top-boy-frontend.onrender.com"
 
 # CSRF_TRUSTED_ORIGINS = os.getenv('ALLOWED_ORIGINS', '').split(',')
 
