e4880f6004541527cbcae4c715f0cdc4db3ca65c
AbdusalomovIbroxim <ibrohimlololowka@gmail.com>
2025-09-06 18:15:22 +0500

    feat(serializers): Добавлены импорты для Football TextChoices
    Добавлены импорты для классов FootballExperience, FootballFrequency, FootballPosition и FootballFormat в serializers.py. Это необходимо для создания API-эндпоинтов, которые будут предоставлять эти данные для выбора на фронтенде.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, AuthViewSet
+from .views import UserViewSet, AuthViewSet, FootballChoicesView, FootballExperienceView, FootballFrequencyView, FootballPositionView, FootballFormatView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='users')
@@ -8,4 +8,13 @@ router.register(r'auth', AuthViewSet, basename='auth')
 
 urlpatterns = [
     path('', include(router.urls)),
+    
+    # Один эндпоинт для всех выборов
+    path('auth/football-choices/', FootballChoicesView.as_view(), name='football-choices'),
+
+    # Отдельные эндпоинты
+    path('auth/football-experience/', FootballExperienceView.as_view(), name='football-experience'),
+    path('auth/football-frequency/', FootballFrequencyView.as_view(), name='football-frequency'),
+    path('auth/football-position/', FootballPositionView.as_view(), name='football-position'),
+    path('auth/football-format/', FootballFormatView.as_view(), name='football-format'),
 ]
