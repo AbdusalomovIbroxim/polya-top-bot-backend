e62ad47ee1fb972e13ef31cee1e03e73701e3a79
AbdusalomovIbroxim <ibrohimlololowka@gmail.com>
2025-09-06 14:44:22 +0500

    add engpoints for owners

--- customers/urls.py
+++ customers/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import OwnerVenueViewSet, OwnerEventViewSet, OwnerPaymentViewSet, OwnerStatisticsView
 
 router = DefaultRouter()
-router.register(r'owner/venues', OwnerVenueViewSet, basename='owner-venues')
+router.register(r'owner/sport-venues', OwnerVenueViewSet, basename='owner-venues')
 router.register(r'owner/events', OwnerEventViewSet, basename='owner-events')
 router.register(r'owner/payments', OwnerPaymentViewSet, basename='owner-payments')
 
