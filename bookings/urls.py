5aa864b73faef53329dc86c0558f610293356cde
AbdusalomovIbroxim <ibrohimlololowka@gmail.com>
2025-09-30 15:45:43 +0500

    add endpoint for confirm payment

--- bookings/urls.py
+++ bookings/urls.py
@@ -14,14 +14,10 @@
 
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import BookingViewSet, TransactionViewSet, telegram_webhook
+from .views import BookingViewSet, TransactionViewSet
 
 router = DefaultRouter()
 router.register(r'bookings', BookingViewSet, basename='booking')
 # router.register(r'transactions', TransactionViewSet, basename='transaction')
 
 urlpatterns = router.urls
-
-urlpatterns += [
-    path("webhooks/telegram/", telegram_webhook, name="telegram_webhook"),
-]
