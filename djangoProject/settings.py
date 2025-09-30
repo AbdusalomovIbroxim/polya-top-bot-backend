5e1b1012c02fc440d47934cf564f9d5035fb892d
AbdusalomovIbroxim <ibrohimlololowka@gmail.com>
2025-09-30 19:41:51 +0500

    Sentry Integration

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -306,7 +306,7 @@ SESSION_COOKIE_SECURE = True
 import sentry_sdk
 
 sentry_sdk.init(
-    dsn="https://236ec593eb9b0372785b126de9a217be@o4510109069672448.ingest.us.sentry.io/4510109071048704",
+    dsn=os.getenv("SENTRY_DSN"),
     # Add data like request headers and IP for users,
     # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
     send_default_pii=True,
