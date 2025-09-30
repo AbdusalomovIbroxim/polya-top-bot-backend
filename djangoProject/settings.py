a461c81ac230f0d86ff78e9c3cde8f78b0ea36f5
AbdusalomovIbroxim <ibrohimlololowka@gmail.com>
2025-09-30 19:48:22 +0500

    Sentry Integration

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -307,7 +307,7 @@ import sentry_sdk
 
 sentry_sdk.init(
     dsn=os.getenv("SENTRY_DSN"),
-    # Add data like request headers and IP for users,
-    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
     send_default_pii=True,
+    environment="development",
+    traces_sample_rate=1.0,
 )
