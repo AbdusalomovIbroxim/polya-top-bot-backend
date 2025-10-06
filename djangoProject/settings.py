16f585662da8f78f47c2a3232d6e451215408785
AbdusalomovIbroxim <ibrohimlololowka@gmail.com>
2025-10-06 17:28:07 +0500

    update django admin panel to unfold

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'unfold.context_processors.unfold',
+                'unfold.context_processors.theme',  # ✅ правильный путь
             ],
         },
     },
